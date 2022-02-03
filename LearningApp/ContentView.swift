452cbc348555598a17d3482dac7283cd98764b18
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-02-03 19:02:14 +0200

    text view created

--- LearningApp/ContentView.swift
+++ LearningApp/ContentView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+            VStack(alignment: .leading) {
+                
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+                        .font(.subheadline)
+                        .foregroundColor(.green)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
             .padding()
     }
 }
