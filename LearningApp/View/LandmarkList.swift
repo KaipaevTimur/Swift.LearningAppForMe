00c0a561ee950e938a9ea7761565f9f90fa6a3eb
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-02-03 22:18:30 +0200

    added LandmarkList view

---
+++ LearningApp/View/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  LearningApp
+//
+//  Created by Admin on 03.02.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
