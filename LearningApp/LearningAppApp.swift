001f2f752bc45d806cc1d7a928656d7734ad88d7
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-02-03 18:30:48 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Admin on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
